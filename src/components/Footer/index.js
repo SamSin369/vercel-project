21cc09f33afdd8500e4224c6d2880b29ae481dc6
Sam Singer <sam.singet79@gmail.com>
2021-04-02 12:15:36 -0700

    initalproject

---
+++ src/components/Footer/index.js
@@ -0,0 +1,89 @@
+import React from "react";
+import {FaFacebook, FaInstagram, FaYoutube, FaTwitter, FaLinkedin} from 'react-icons/fa'
+import {
+  FooterContainer,
+  FooterWrap,
+  FooterLinksContainer,
+  FooterLinksWrapper,
+  FooterLinkItems,
+  FooterLinkTitle,
+  FooterLink,
+  SocialMedia,
+  SocialMediaWrap,
+  SocialLogo,
+  WebsiteRights,
+  SocialIcons,
+  SocialIconLink,
+} from "./FooterElements";
+
+
+const Footer = () => {
+  return (
+    <FooterContainer>
+      <FooterWrap>
+        <FooterLinksContainer>
+          <FooterLinksWrapper>
+            <FooterLinkItems>
+              <FooterLinkTitle> About Us</FooterLinkTitle>
+
+              <FooterLink to="/signin">How It Works</FooterLink>
+              <FooterLink to="/signin">Testimonials</FooterLink>
+              <FooterLink to="/signin">Careers</FooterLink>
+              <FooterLink to="/signin">Investors</FooterLink>
+            </FooterLinkItems>
+            <FooterLinkItems>
+              <FooterLinkTitle> About Us</FooterLinkTitle>
+
+              <FooterLink to="/signin">How It Works</FooterLink>
+              <FooterLink to="/signin">Testimonials</FooterLink>
+              <FooterLink to="/signin">Careers</FooterLink>
+              <FooterLink to="/signin">Investors</FooterLink>
+            </FooterLinkItems>
+          </FooterLinksWrapper>
+          <FooterLinksWrapper>
+            <FooterLinkItems>
+              <FooterLinkTitle> About Us</FooterLinkTitle>
+
+              <FooterLink to="/signin">How It Works</FooterLink>
+              <FooterLink to="/signin">Testimonials</FooterLink>
+              <FooterLink to="/signin">Careers</FooterLink>
+              <FooterLink to="/signin">Investors</FooterLink>
+            </FooterLinkItems>
+            <FooterLinkItems>
+              <FooterLinkTitle> About Us</FooterLinkTitle>
+
+              <FooterLink to="/signin">How It Works</FooterLink>
+              <FooterLink to="/signin">Testimonials</FooterLink>
+              <FooterLink to="/signin">Careers</FooterLink>
+              <FooterLink to="/signin">Investors</FooterLink>
+            </FooterLinkItems>
+          </FooterLinksWrapper>
+        </FooterLinksContainer>
+        <SocialMedia>
+          <SocialMediaWrap>
+            <SocialLogo to="/">dolla</SocialLogo>
+            <WebsiteRights>
+              dolla {new Date().getFullYear()} Website Rights
+            </WebsiteRights>
+            <SocialIcons>
+              <SocialIconLink href="/" target="_blank" aria-label="Facebook">
+                <FaFacebook />
+              </SocialIconLink>
+              <SocialIconLink href="/" target="_blank" aria-label="Facebook">
+                <FaFacebook />
+              </SocialIconLink>
+              <SocialIconLink href="/" target="_blank" aria-label="Facebook">
+                <FaFacebook />
+              </SocialIconLink>
+              <SocialIconLink href="/" target="_blank" aria-label="Facebook">
+                <FaFacebook />
+              </SocialIconLink>
+            </SocialIcons>
+          </SocialMediaWrap>
+        </SocialMedia>
+      </FooterWrap>
+    </FooterContainer>
+  );
+};
+
+export default Footer;
---
+++ src/components/infoSection/Data.js
@@ -0,0 +1,51 @@
+export const homeObjOne = {
+  id: "about",
+  lightBg: true,
+  lightText: false,
+  lightTextDesc: false,
+  topLine: "Custom Herbs For Your Health",
+  headline: "Hand Crafted Personalized Medicine",
+  description:
+    "We Craft Our Herbs Straight From Mother Nature! All herbs are harvested sustainably from the wild.",
+  buttonLabel: "Learn More",
+  imgStart: false,
+  img: require("../../images/svg-1.svg"),
+  alt: "Car",
+  dark: true,
+  primary: true,
+  darkText: true,
+};
+export const homeObjTwo = {
+  id: "discover",
+  lightBg: false,
+  lightText: true,
+  lightTextDesc: false,
+  topLine: "We Go Above And Beyond",
+  headline: "First of its kind market for custom powerful herbal medicines",
+  description:
+    "Get acesdajasldkfjak aghalknlabjlkabljkasadjkladvjklavjl;kdasjl;abjklalabsdkljakslbj",
+  buttonLabel: "Learn More",
+  imgStart: true,
+  img: require("../../images/svg-2.svg"),
+  alt: "Car",
+  dark: false,
+  primary: false,
+  darkText: true,
+};
+export const homeObjThree = {
+  id: "about",
+  lightBg: false,
+  lightText: true,
+  lightTextDesc: true,
+  topLine: "Premium Bank",
+  headline: "Unlimited Transaction With Zero Fees",
+  description:
+    "Get acesdajasldkfjak aghalknlabjlkabljkasadjkladvjklavjl;kdasjl;abjklalabsdkljakslbj",
+  buttonLabel: "Get started",
+  imgStart: false,
+  img: require("../../images/svg-3.svg"),
+  alt: "Car",
+  dark: true,
+  primary: true,
+  darkText: false,
+};
